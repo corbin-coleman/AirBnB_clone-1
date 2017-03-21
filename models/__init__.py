c0c7bfe2743610551805d93aa54f8617b094188b
corbin-coleman <corbincol@hotmail.com>
2017-03-21 08:54:41 +0000

    Attempting to add MySQL functionality

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+from os import environ
 from models.base_model import BaseModel
 from models.user import User
 from models.amenity import Amenity
@@ -6,8 +7,14 @@ from models.city import City
 from models.place import Place
 from models.state import State
 from models.review import Review
-from models.engine import file_storage
 
 
-storage = file_storage.FileStorage()
-storage.reload()
+
+if environ.get('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
---
+++ models/engine/db_storage.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+from os import environ
+from models import *
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models.base_model import Base
+
+class DBStorage:
+    __engine = None
+    __session = None
+
+    def init(self):
+        user = environ.get('HBNB_MYSQL_USER')
+        passwd = environ.get('HBNB_MYSQL_PWD')
+        host = environ.get('HBNB_MYSQL_HOST')
+        database = environ.get('HBNB_MYSQL_ENV')
+        mysql = "mysql+mysqldb://{}:{}@{}/{}".format(passwd, user, host,
+                                                     database)
+        self.__engine = create_engine(mysql)
+        Base.metadata.create_all(self.__engine)
+
+    def all(self, cls=None):
+        db_store = {}
+        classes = ['User', 'State', 'City', 'Amenity', 'Place', 'Review']
+        if cls and cls in classes:
+            for obj in self.__session.query(cls):
+                db_store.update({obj.id: obj})
+        elif cls is None:
+            for class_name in classes:
+                for obj in self.__session.query(class_name):
+                    db_store.update({obj.id: obj})
+        return(db_store)
+
+    def new(self, obj):
+        self.__session.add(obj)
+
+    def save(self):
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        Session = sessionmaker(bind=self.__engine)
+        self.__session = Session()
