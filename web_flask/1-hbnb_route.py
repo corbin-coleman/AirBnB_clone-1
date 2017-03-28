845525923a51ab762c02fb8c4ac10755e1da376a
corbin-coleman <corbincol@hotmail.com>
2017-03-28 22:37:04 +0000

    Remove extra comma

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -2,13 +2,16 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
-    return 'Hello, HBNB!'
+    return 'Hello HBNB!'
+
 
 @app.route('/hbnb')
 def hbnb():
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -2,28 +2,34 @@
 from flask import Flask, abort
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
-    return 'Hello, HBNB!'
+    return 'Hello HBNB!'
+
 
 @app.route('/hbnb')
 def hbnb():
     return 'HBNB'
 
+
 @app.route('/c/<text>')
 def c_is(text):
     return 'C {}'.format(text.replace('_', ' '))
+
 
 @app.route('/python/')
 @app.route('/python/<text>')
 def python_is(text='is cool'):
     return 'Python {}'.format(text.replace('_', ' '))
 
+
 @app.route('/number/<n>')
 def is_number(n):
     if n.isdigit():
         return '{} is a number'.format(n)
     abort(404)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
