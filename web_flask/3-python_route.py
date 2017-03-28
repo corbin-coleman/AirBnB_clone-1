6172ee1b67865cbf0f082f3cc614f1470d4099bd
corbin-coleman <corbincol@hotmail.com>
2017-03-28 20:21:15 +0000

    Task 3 - Variable URL with default value

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, HBNB!'
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+@app.route('/c/<text>')
+def c_is(text):
+    return 'C {}'.format(text.replace('_', ' '))
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_is(text='is cool'):
+    return 'Python {}'.format(text.replace('_', ' '))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
