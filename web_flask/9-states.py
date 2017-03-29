097dacd45285790955331c22126d7169b2462f83
corbin-coleman <corbincol@hotmail.com>
2017-03-29 05:05:32 +0000

    Task 10 - List Cities by State id w/ dynamic HTML

---
+++ web_flask/9-states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+from models import storage
+app = Flask(__name__)
+
+
+@app.route('/states')
+def state_list():
+    return render_template('7-states_list.html',
+                           states=storage.all('State').values())
+
+@app.route('/states/<id>')
+def state_id(id):
+    state = None
+    if id in storage.all('State').keys():
+        state = storage.all('State')[id]
+    return render_template('9-states.html', state=state)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
