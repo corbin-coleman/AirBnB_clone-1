38688981827364fffb487ab6bb38c3960a3c1a73
corbin-coleman <corbincol@hotmail.com>
2017-03-29 04:25:05 +0000

    Pep8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,9 +3,12 @@ from flask import Flask, render_template
 from models import storage
 app = Flask(__name__)
 
+
 @app.route('/states_list/')
 def state_list():
-    return render_template('7-states_list.html', states=storage.all('State').values())
+    return render_template('7-states_list.html',
+                           states=storage.all('State').values())
+
 
 @app.teardown_appcontext
 def teardown(exception):
